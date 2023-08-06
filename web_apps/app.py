8d1c86389d59b46c818bce07b1514b65c17d7118
Khalid Ibrahim <kibrahim@ANC0898.local>
2023-08-06 14:07:02 +0100

    add existing file

---
+++ web_apps/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from weather_app import main as get_weather
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    if request.method == 'POST':
+        city = request.form['cityName']
+        country = request.form['countryName']
+        data = (get_weather(city, country))
+
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
